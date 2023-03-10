1c3b46e1a228ee8eedc500391c6640dbfdc9875e
guiBrisson <guibrisson@hotmail.com>
2023-03-10 17:39:29 -0300

    define base

--- .svelte-kit/output/client/_app/immutable/chunks/1.b6c1c737.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.4de595fb.js";export{t as component};
--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -4,7 +4,7 @@ export const manifest = {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":{"file":"_app/immutable/entry/start.8d26eb5a.js","imports":["_app/immutable/entry/start.8d26eb5a.js","_app/immutable/chunks/index.e4b580e2.js","_app/immutable/chunks/singletons.eb821a5b.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.b3905ef9.js","imports":["_app/immutable/entry/app.b3905ef9.js","_app/immutable/chunks/index.e4b580e2.js"],"stylesheets":[],"fonts":[]}},
+		client: {"start":{"file":"_app/immutable/entry/start.e0983942.js","imports":["_app/immutable/entry/start.e0983942.js","_app/immutable/chunks/index.e4b580e2.js","_app/immutable/chunks/singletons.a37ab562.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.833ec4fb.js","imports":["_app/immutable/entry/app.833ec4fb.js","_app/immutable/chunks/index.e4b580e2.js"],"stylesheets":[],"fonts":[]}},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
